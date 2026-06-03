e31c13b8c79fee5235964b4186061c70e5f59be8
Bartek Moczulski <12998104+bartekmo@users.noreply.github.com>
2026-06-03 17:03:58 +0200

    feat: user TAPs manage privs

--- infra/terraform-seed/entraid.tf
+++ infra/terraform-seed/entraid.tf
@@ -47,3 +47,28 @@ resource "azurerm_role_assignment" "app_id_subscription_reader" {
   principal_id         = azurerm_user_assigned_identity.ctf_app.principal_id
 }
 */
+
+
+## user managment role assignments for creating and pulling TAPs
+data "azuread_service_principal" "graph" {
+  client_id = "00000003-0000-0000-c000-000000000000"
+}
+
+resource "azuread_app_role_assignment" "tap_readwrite" {
+  app_role_id = [
+    for r in data.azuread_service_principal.graph.app_roles :
+    r.id if r.value == "UserAuthenticationMethod.ReadWrite.All"
+  ][0]
+  principal_object_id = azurerm_user_assigned_identity.ctf_app.principal_id
+  resource_object_id  = data.azuread_service_principal.graph.object_id
+}
+
+# User.Read.All — needed to enumerate users by UPN filter
+resource "azuread_app_role_assignment" "user_read" {
+  app_role_id = [
+    for r in data.azuread_service_principal.graph.app_roles :
+    r.id if r.value == "User.Read.All"
+  ][0]
+  principal_object_id = azurerm_user_assigned_identity.ctf_app.principal_id
+  resource_object_id  = data.azuread_service_principal.graph.object_id
+}
